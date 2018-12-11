9277ff9099bc7401c1a3037fdcec74eafe7c36aa
Thymon Arens <contact@thymonarens.nl>
2018-12-11 11:10:14 +0100

    permissions are now translated

---
+++ NetCoreTemplate.Providers/General/EntityLabelProvider.cs
@@ -0,0 +1,14 @@
+﻿namespace NetCoreTemplate.Providers.General
+{
+    using NetCoreTemplate.DAL.Models.Translation;
+    using NetCoreTemplate.Providers.Base;
+    using NetCoreTemplate.SharedKernel.Interfaces.PersistenceLayer;
+
+    public sealed class EntityLabelProvider : BaseProvider<EntityLabel>
+    {
+        public EntityLabelProvider(IPersistenceLayer persistence)
+            : base(persistence)
+        {
+        }
+    }
+}
--- NetCoreTemplate.ViewModelProcessors/Controllers/Role/RoleLoader.cs
+++ NetCoreTemplate.ViewModelProcessors/Controllers/Role/RoleLoader.cs
@@ -6,6 +6,8 @@
     using Microsoft.EntityFrameworkCore;
 
     using NetCoreTemplate.DAL.Models.General;
+    using NetCoreTemplate.DAL.Models.Translation;
+    using NetCoreTemplate.Providers.Interfaces;
     using NetCoreTemplate.Providers.Interfaces.General;
     using NetCoreTemplate.SharedKernel.Extensions;
     using NetCoreTemplate.SharedKernel.ServiceContainer;
@@ -17,12 +19,14 @@
     {
         private readonly IRoleProvider roleProvider;
         private readonly IPermissionProvider permissionProvider;
+        private readonly IBaseProvider<EntityLabel> entityLabelProvider;
 
         public RoleLoader(IServiceContainer serviceContainer)
             : base(serviceContainer)
         {
             this.roleProvider = serviceContainer.GetService<IRoleProvider>();
             this.permissionProvider = serviceContainer.GetService<IPermissionProvider>();
+            this.entityLabelProvider = serviceContainer.GetService<IBaseProvider<EntityLabel>>();
         }
 
         protected override TranslationSettings TranslationSettings => new TranslationSettings("Dashboard", "Role");
@@ -82,7 +86,7 @@
                 new PermissionViewModel
                 {
                     Id = permission.Id,
-                    Action = permission.Action,
+                    Action = GetEntityTranslation(permission.EntityLabelDefinition_Id),
                     Active = activePermissions.Any(y => y.Id == permission.Id)
                 });
 
@@ -104,12 +108,25 @@
                 new PermissionViewModel
                 {
                     Id = permission.Id,
-                    Action = permission.Action,
+                    Action = GetEntityTranslation(permission.EntityLabelDefinition_Id),
                     Active = permissions.Any(y => y.Id == permission.Id)
                 });
 
             viewModel.Permissions = new List<PermissionViewModel>();
             viewModel.Permissions.AddRange(permissionViewModels);
+        }
+
+        private string GetEntityTranslation(int entityLabelDefinitionId)
+        {
+            var languageId = LanguageId;
+
+            var entityTranslationLabel = entityLabelProvider
+                .GetEntity(x => x.EntityLabelDefinition_Id == entityLabelDefinitionId &&
+                                x.Language_Id == languageId);
+
+            return entityTranslationLabel.IsNullOrDefault()
+                       ? string.Empty
+                       : entityTranslationLabel.Label;
         }
     }
 }
